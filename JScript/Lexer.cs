2b28eb636b13b8770412823b3b36f887340d5cca
Bzway <zhumingwu@hotmail.com>
2017-12-04 23:09:23 +0800

    update

---
+++ JScript/Lexer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace JScript
+{
+
+    public class Lexer
+    {
+
+    } 
+    public class Position
+    {
+        public int Line { get; set; }
+        public int Col { get; set; }
+    }
+    public class Token
+    {
+        public Position Position { get; set; }
+        public string Value { get; set; }
+        public TokenType Type { get; set; }
+    }
+    public class TokenDefinition
+    {
+        public TokenType Type { get; set; }
+        public Regex Regex { get; set; }
+
+    }
+    public enum TokenType
+    {
+        Char,
+        Symbol,
+        Number,
+        Decimal,
+        Identifier,
+        Keyword,
+        QuotedString,
+        WhiteSpace,
+        EndOfLine,
+        Comment,
+        Start,
+        End
+    }
+}
---
+++ JScript/Parser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace JScript
+{
+
+    
+    public class Parser
+    {
+
+    }
+    public class SyntaxTree
+    {
+
+    }
+
+    public class ASTNode
+    { } 
+  
+}
